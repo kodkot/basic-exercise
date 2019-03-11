099928fd3646a84fb2156cf485b7a43c69c81c9a
Paulian <chyzyjez@gmail.com>
2019-03-11 21:50:52 +0100

    added values generated randomly; added cell numbers;

--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -1,10 +1,16 @@
+import java.util.Random;
+
 public class Table {
     public static void main (String[]args){
 
         int[] table = new int[10];
+        Random generate = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            int number = generate.nextInt(1000);
+            table[i] = number;
+        }
         for (int i = 0; i < 10; i++)
-            table[i] = 10 - i;
-        for (int i = 0; i < 10; i++)
-            System.out.println("Next cell is: " + table[i]);
+            System.out.println("Value for cell " + i + " is: " + table[i]);
     }
 }
