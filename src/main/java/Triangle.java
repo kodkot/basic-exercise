fcb9e82b3694a63f1ec19d8d98cc5271696ef0b6
Paulian <chyzyjez@gmail.com>
2019-03-11 13:33:50 +0100

    added user-given size of triangle

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Triangle {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; ++i) {
+
+        System.out.println("What size do you want to have your star triangle?");
+
+        Scanner scanner = new Scanner(System.in);
+        int size = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < size; ++i) {
             for (int j = 0; j < i; ++j) {
                 System.out.print("*");
             }
