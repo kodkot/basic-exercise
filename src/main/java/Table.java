800fa919823a6992e987ae8e08d2d5c886614fb1
Paulian <chyzyjez@gmail.com>
2019-03-13 20:12:40 +0100

    added switch for finding index or value from the array

--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -1,16 +1,41 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Table {
-    public static void main (String[]args){
+    public static void main(String[] args) {
 
-        int[] table = new int[10];
+        int i;
+        int table [] = new int[10];
         Random generate = new Random();
 
-        for (int i = 0; i < 10; i++) {
+        System.out.println("What would you like to do?" + '\n' + "1. Find value on basis of the index." + '\n'
+                + "2. Find specific value.");
+        Scanner scanner = new Scanner(System.in);
+        int choice = Integer.parseInt(scanner.nextLine());
+
+        for (i = 0; i < 10; i++) {
             int number = generate.nextInt(1000);
             table[i] = number;
         }
-        for (int i = 0; i < 10; i++)
-            System.out.println("Value for cell " + i + " is: " + table[i]);
+//        }
+//        for (i = 0; i < 10; i++)
+//            System.out.println("Value for cell " + i + " is: " + table[i]);
+
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Give index number");
+                    int index = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Value for index " + index + " is " + table[index]);
+                    break;
+
+                case 2:
+                    System.out.println("What is the value?");
+                    int value = Integer.parseInt(scanner.nextLine());
+                    value = table[i];
+                    System.out.println("Value " + value + "exists in the table with index " + i);
+                    break;
+
+        }
     }
 }
